04ddae3c5a61e8d45392c5e38bea077a6d4dd2df
Mario Bodom <mariotecladista666@gmail.com>
2022-05-13 10:22:59 +0200

    añado las clases que faltan y quito la referencia

---
+++ src/GrupoTrabajo.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class GrupoTrabajo {
+    ArrayList<Trabajo> trabajos = new ArrayList<>();
+    private final String REP_CHA = "Reparacion chapa ";
+    private final String REP_MECA = "Reparacion mecánica ";
+    private final String REVISION = "Revisión ";
+
+
+    public void addTrabajo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Que tipo de trabajo quiere añadir?\n1. Reparacion chapa\n2. Reparacion pintura\n3. Revisión");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                sc.nextLine();
+                System.out.println("Indica una breve descripcion del trabajo (el tipo ya está puesto)");
+                trabajos.add(new Trabajo(REP_CHA + sc.nextLine(), trabajos.size(), choice));
+                System.out.println("ID del Trabajo: "+ (trabajos.size()-1));
+                break;
+            case 2:
+                sc.nextLine();
+                System.out.println("Indica una breve descripcion del trabajo (el tipo ya está puesto)");
+                trabajos.add(new Trabajo(REP_MECA + sc.nextLine(), trabajos.size(), choice));
+                System.out.println("ID del Trabajo: "+ (trabajos.size()-1));
+                break;
+            case 3:
+                sc.nextLine();
+                System.out.println("Indica una breve descripcion del trabajo (el tipo ya está puesto)");
+                trabajos.add(new Trabajo(REVISION + sc.nextLine(), trabajos.size(), choice));
+                System.out.println("ID del Trabajo: "+ (trabajos.size()-1));
+                break;
+        
+            default:
+                System.out.println("La elección no es correcta");
+                break;
+        }
+    }
+
+    public void aumentaHoras() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es el número del trabajo para aumentar las horas?");
+        int opcion = sc.nextInt();
+        for (int i = 0; i < trabajos.size(); i++) {
+            if (opcion == i && !trabajos.get(i).getFinalizado()) {
+                System.out.println("Escribe el número de horas");
+                int horas = sc.nextInt();
+                while (horas <= 0) {
+                    System.out.println("Las horas no pueden ser negativas");
+                    horas = sc.nextInt();
+                }
+                trabajos.get(i).setHoras(horas);
+            } else {
+                if (trabajos.get(i).getFinalizado()) {
+                    System.out.println("El trabajo está finalizado y no se pueden aumentar las horas.");
+                } else {
+                    System.out.println("El ID del trabajo no existe");
+                }
+            }
+        }
+    }
+
+    public void aumentaCoste() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es el número del trabajo para aumentar el coste?");
+        int opcion = sc.nextInt();
+        for (int i = 0; i < trabajos.size(); i++) {
+            if (opcion == i && !trabajos.get(i).getFinalizado()) {
+                System.out.println("Escribe el coste de los materiales");
+                double precioMaterial = sc.nextDouble();
+                while (precioMaterial <= 0) {
+                    System.out.println("El precio de los materiales no puede ser negativo");
+                    precioMaterial = sc.nextInt();
+                }
+                trabajos.get(i).setPrecioMaterial(precioMaterial);
+            } else {
+                if (trabajos.get(i).getFinalizado()) {
+                    System.out.println("El trabajo está finalizado y no se puede aumentar el precio de los materiales.");
+                } else {
+                    System.out.println("El ID del trabajo no existe");
+                }
+            }
+        }
+    }
+
+    public void finalizaTrabajo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es el número del trabajo para finalizar?");
+        int opcion = sc.nextInt();
+        for (int i = 0; i < trabajos.size(); i++) {
+            if (opcion == i && !trabajos.get(i).getFinalizado()) {
+                trabajos.get(i).setFinalizado(true);;
+            } else {
+                if (trabajos.get(i).getFinalizado()) {
+                    System.out.println("El trabajo está finalizado y no se puede volver a finalizar.");
+                } else {
+                    System.out.println("El ID del trabajo no existe");
+                }
+            }
+        }
+    }
+
+    public void muestraTrabajo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es el número del trabajo para mostrar?");
+        int opcion = sc.nextInt();
+        for (int i = 0; i < trabajos.size(); i++) {
+            if (opcion == i) {
+                System.out.println(trabajos.get(i).toString());
+            } else {
+                System.out.println("El trabajo no existe");
+            }
+        }
+    }
+    
+    
+}
---
+++ src/Trabajo.java
@@ -0,0 +1,89 @@
+public class Trabajo {
+    private int idTrabajo;
+    private String descripcion;
+    private int horas;
+    private boolean finalizado = false;
+    private double precioMaterial;
+    private int tipoTrabajo;
+    private double precioTotal;
+
+    public Trabajo(String descripcion, int idTrabajo, int tipoTrabajo){
+        this.descripcion = descripcion;
+        this.idTrabajo = idTrabajo;
+        this.tipoTrabajo = tipoTrabajo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdTrabajo() {
+        return idTrabajo;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas += horas;
+    }
+
+    public double getPrecioMaterial() {
+        return precioMaterial;
+    }
+
+    public void setPrecioMaterial(double precioMaterial) {
+        this.precioMaterial += precioMaterial;
+    }
+
+    public int getTipoTrabajo() {
+        return tipoTrabajo;
+    }
+
+    public void setTipoTrabajo(int tipoTrabajo) {
+        this.tipoTrabajo = tipoTrabajo;
+    }
+
+    public boolean getFinalizado(){
+        return finalizado;
+    }
+
+    public void setFinalizado(boolean finalizado){
+        this.finalizado = finalizado;
+    }
+
+    public double getPrecioTotal() {
+        setPrecioTotal();
+        return precioTotal;
+    }
+
+    public void setPrecioTotal() {
+        switch (getTipoTrabajo()) {
+            case 1:
+                this.precioTotal = this.horas*30 + this.precioMaterial*1.3;
+                break;
+            case 2:
+                this.precioTotal = this.horas*30 + this.precioMaterial*1.1;
+                break;
+            case 3:
+                this.precioTotal = this.horas*30 + 20;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        if (finalizado) {
+            return "El trabajo con id " + getIdTrabajo() + " es de tipo "+ getDescripcion() + " con un precio de " + getPrecioTotal() + " y una duración de "+ getHoras()+" horas, está finalizado";
+            
+        }else return "El trabajo con id " + getIdTrabajo() + " es de tipo "+ getDescripcion() + " con un precio de " + getPrecioTotal() + " y una duración de "+ getHoras()+" horas, está en curso";
+    }
+}
